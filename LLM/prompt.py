40f67f99e03c3c8fa24131d6caa9f8698f432a77
Jan-Philip Loos <maxdaten@gmail.com>
2023-05-12 01:28:36 +0200

    Added more fun: Add Emojis to options

--- LLM/prompt.py
+++ LLM/prompt.py
@@ -5,7 +5,9 @@ def get_adventure_prompt(language: str = "de") -> List[dict]:
     adventure_rules = [{"role": "system", "content": "You are a game master for a text adventure."},
                   {"role": "system", "content": "The user is the player in this text adventure."},
                   {"role": "system", "content": """Der Spieler will eine Spannende Geschichte spielen. Ein Spiel dauert 10 Interaktionen. 
+                  Die Geschichte wird in der 3. Person erzählt. Der Spieler ist ein Charakter in der Geschichte.
                   In jeder Interaktion hat der Spieler 3 Optionen (A,B,C) aus denen der Spieler wählen muss wie die Geschichte weitergeht. 
+                  Beschreibe die Optionen mit bis zu zwei Emojis und Text.
                   Du wartest bei jeder Interaktion darauf, dass der Spieler eine Option ausgewählt hat. Der Spieler kennt immer nur die aktuelle Interaktion 
                   und neue Interaktionen werden erst generiert, wenn der Spieler eine Option gewählt hat."""},
                   ]
