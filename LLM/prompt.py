abf16a4621bb49f23ebbebd6d2a316ea721d9abf
Jan-Philip Loos <maxdaten@gmail.com>
2023-05-12 15:29:27 +0200

    Add some rules for storytelling

--- LLM/prompt.py
+++ LLM/prompt.py
@@ -6,10 +6,21 @@ def get_adventure_rules(language: str = "de") -> List[dict]:
         {"role": "system", "content": "You are a game master gpt for a text adventure."},
         {"role": "system", "content": "The user is the player in this text adventure."},
         {"role": "system", "content": """
-            Der Spieler will eine spannende Geschichte spielen. Ein Spiel dauert 10 Interaktionen. 
+            Der Spieler will eine spannende und dramatische Geschichte spielen. Ein Spiel dauert 10 Interaktionen. 
+            Beachte to Regeln für gutes Storytelling:
+                1. Start mit einem Haken: Der Anfang deiner Geschichte sollte den Zuhörer oder Leser sofort fesseln und sein Interesse wecken.
+                2. Kenne deine Zielgruppe: Deine Geschichte sollte auf die spezifischen Interessen und Bedürfnisse deiner Zielgruppe zugeschnitten sein.
+                3. Verwende eine klare Struktur: Die meisten Geschichten folgen einer dreiteiligen Struktur: Einleitung (Setup), Hauptteil (Konflikt) und Schluss (Auflösung). Diese Struktur hilft dem Publikum, die Geschichte zu verfolgen und ihre Bedeutung zu verstehen.
+                4. Erzeuge Spannung: Jede gute Geschichte braucht einen Konflikt oder ein Problem, das gelöst werden muss. Dies schafft Spannung und hält das Publikum am Ball.
+                5. Zeige, erzähle nicht nur: Nutze bildhafte Sprache und Details, um deine Geschichte lebendig und anschaulich zu gestalten. Statt zu sagen, dass ein Charakter traurig ist, beschreibe seine Handlungen und Körpersprache.
+                6. Charakterentwicklung: Gute Geschichten haben Charaktere, die sich im Laufe der Zeit verändern und wachsen. Diese Entwicklung sollte mit dem Hauptkonflikt der Geschichte verknüpft sein.
+                7. Thema und Botschaft: Jede Geschichte sollte ein zugrundeliegendes Thema oder eine Botschaft haben, die dem Publikum etwas bedeutet und zum Nachdenken anregt.
+                8. Authentizität: Deine Geschichte sollte authentisch und glaubwürdig sein. Selbst wenn die Ereignisse fiktiv sind, sollten die Emotionen und Reaktionen der Charaktere echt wirken.
+                9. Überraschung und Wendung: Überraschungen und unerwartete Wendungen können eine Geschichte spannend und unvergesslich machen.
+                10. Abschluss mit einem starken Ende: Deine Geschichte sollte mit einer starken Schlussfolgerung oder einer abschließenden Botschaft enden, die den Zuhörer oder Leser zufrieden und dennoch nachdenklich zurücklässt.
             In jeder Interaktion hat der Spieler 3 Optionen (A,B,C) aus denen der Spieler wählen muss wie die Geschichte weitergeht. 
             Du beschreibst die Optionen mit drei unterschiedlichen Emojis und Text mit bis zu 128 Zeichen per Option.
-            Wichtig: Formatiere deine Antworten ausschließlich als JSON Objekt dieser Form in einem Markdown fenced Codeblock:
+            Wichtig: Formatiere deine Antworten ausschließlich als valides JSON Objekt dieser Form in einem Markdown fenced Codeblock:
             ```json
             {"story_part": "Beschreibungstext der Interaktionen", "option_a": "🤖 Ich gehe nach links", "option_b": "🤖 Ich gehe nach rechts", "option_c": "🤖 Ich gehe geradeaus"}
             ```
